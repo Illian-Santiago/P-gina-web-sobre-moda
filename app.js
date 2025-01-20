a68fc3b34f70be3e4977c7c0679cefea32eb52ae
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-20 14:58:45 +0000

    Libro agregado

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// Contenedor donde se cargará el contenido dinámico
 const app = document.getElementById('app');
 
-// HTML de cada página como plantilla
 const routes = {
     '/': `
     <section class="hero-section">
@@ -9,10 +7,6 @@ const routes = {
         <h1>Descubre lo último en <span class="highlight">Moda</span></h1>
         <p>Explora las tendencias más destacadas y las colecciones exclusivas para cada temporada. Vive la moda a tu estilo.</p>
         <a href="#/colecciones" class="cta">Ver colecciones →</a>
-      </div>
-      <div class="social-icons">
-        <a href="#"><img src="instagram-icon.png" alt="Instagram"></a>
-        <a href="#"><img src="pinterest-icon.png" alt="Pinterest"></a>
       </div>
     </section>
   `,
@@ -23,16 +17,9 @@ const routes = {
       <img src="images/tendencias.jpg" alt="Tendencias" class="responsive-img">
     </section>
   `,
-    '/colecciones': `
+    '/libro': `
     <section class="colecciones-section">
-      <h1>Colecciones Exclusivas</h1>
-      <p>Explora nuestras colecciones para cada temporada. Diseños únicos y estilos modernos.</p>
-      <div class="collection-grid">
-        <div class="collection-item"><img src="images/verano.jpg" alt="Verano"></div>
-        <div class="collection-item"><img src="images/invierno.jpg" alt="Invierno"></div>
-        <div class="collection-item"><img src="images/otoño.jpg" alt="Otoño"></div>
-        <div class="collection-item"><img src="images/primavera.jpg" alt="Primavera"></div>
-      </div>
+    <iframe src="https://online.fliphtml5.com/chyzd/rezo/" frameborder="0"></iframe>
     </section>
   `,
     '/contacto': `
@@ -52,15 +39,12 @@ const routes = {
   `
 };
 
-// Función para cargar la ruta según el hash
 function loadRoute() {
     const hash = window.location.hash || '#/';
     const route = hash.slice(1);
     app.innerHTML = routes[route] || '<h1>Página no encontrada</h1>';
 }
 
-// Escuchar cambios en el hash de la URL
 window.addEventListener('hashchange', loadRoute);
 
-// Cargar la ruta inicial al cargar la página
 loadRoute();
