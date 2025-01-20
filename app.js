03a910fb3e813151c6804f3dc3c9496228e68f32
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-20 15:28:23 +0000

    Mejoras

--- app.js
+++ app.js
@@ -11,15 +11,35 @@ const routes = {
     </section>
   `,
     '/tendencias': `
-    <section class="tendencias-section">
-      <h1>Tendencias</h1>
-      <p>Descubre las tendencias de moda más populares este año, desde estilos minimalistas hasta estampados vibrantes.</p>
-      <img src="images/tendencias.jpg" alt="Tendencias" class="responsive-img">
-    </section>
+<section class="tendencias-section">
+  <h1>Tendencias</h1>
+  <ul>
+    <li>
+      <strong>Transformación Digital:</strong>
+      <p>La integración de tecnologías avanzadas como la inteligencia artificial, el aprendizaje automático y la automatización está revolucionando la manera en que las empresas operan.</p>
+      <img src="imagen1.jpg" alt="Imagen 1">
+    </li>
+    <li>
+      <strong>Sostenibilidad como Prioridad:</strong>
+      <p>Los consumidores y empresas están adoptando prácticas sostenibles, desde productos ecológicos hasta procesos que reducen la huella de carbono.</p>
+      <img src="imagen2.jpg" alt="Imagen 2">
+    </li>
+    <li>
+      <strong>Enfoque en la Experiencia del Usuario (UX):</strong>
+      <p>Brindar experiencias personalizadas, intuitivas y fluidas es el centro de atención.</p>
+      <img src="imagen3.jpg" alt="Imagen 3">
+    </li>
+  </ul>
+</section>
+
   `,
     '/libro': `
     <section class="colecciones-section">
+    <section class="colecciones-section">
+  <div class="iframe-container">
     <iframe src="https://online.fliphtml5.com/chyzd/rezo/" frameborder="0"></iframe>
+  </div>
+</section>
     </section>
   `,
     '/contacto': `
@@ -48,3 +68,15 @@ function loadRoute() {
 window.addEventListener('hashchange', loadRoute);
 
 loadRoute();
+
+
+function textToSpeech() {
+    const text =
+        `
+        Bienvenido a Moda Moderna, donde encontrarás lo último en moda y tendencias.
+        Explora nuestras secciones en la parte de arriba a la derecha: tendencias, un libro donde poder saber un poco más de la moda y si lo necesitas contactar con nosotros.
+        En la parte de abajo del todo, en el centro, encontratas algunos enlaces de interes.
+        `;
+    const utterance = new SpeechSynthesisUtterance(text)
+    window.speechSynthesis.speak(utterance);
+}
